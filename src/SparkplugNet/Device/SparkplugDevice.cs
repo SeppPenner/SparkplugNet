b7feaddfe5435685818a789606928ee9aa8558a4
SeppPenner <HansM2013@gmx.de>
2021-01-31 20:09:44 +0100

    Added device.

---
+++ src/SparkplugNet/Device/SparkplugDevice.cs
@@ -0,0 +1,213 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SparkplugDevice.cs" company="Hämmer Electronics">
+// The project is licensed under the MIT license.
+// </copyright>
+// <summary>
+//   A class that handles a Sparkplug device.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SparkplugNet.Device
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MQTTnet;
+    using MQTTnet.Client;
+    using MQTTnet.Client.Options;
+    using MQTTnet.Formatter;
+    using MQTTnet.Protocol;
+
+    using SparkplugNet.Enumerations;
+
+    /// <inheritdoc cref="SparkplugBase"/>
+    /// <summary>
+    /// A class that handles a Sparkplug application.
+    /// </summary>
+    /// <seealso cref="SparkplugBase"/>
+    public class SparkplugDevice : SparkplugBase
+    {
+        /// <summary>
+        /// The will message.
+        /// </summary>
+        private MqttApplicationMessage? willMessage;
+
+        /// <summary>
+        /// The device online message.
+        /// </summary>
+        private MqttApplicationMessage? deviceOnlineMessage;
+
+        /// <inheritdoc cref="SparkplugBase"/>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SparkplugDevice"/> class.
+        /// </summary>
+        /// <param name="version">The version.</param>
+        /// <param name="nameSpace">The namespace.</param>
+        /// <seealso cref="SparkplugBase"/>
+        public SparkplugDevice(SparkplugVersion version, SparkplugNamespace nameSpace)
+            : base(version, nameSpace)
+        {
+        }
+
+        /// <summary>
+        /// Starts the Sparkplug device.
+        /// </summary>
+        /// <param name="options">The configuration option.</param>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        public async Task Start(SparkplugDeviceOptions options)
+        {
+            // Load messages
+            this.LoadMessages(options);
+
+            // Add handlers
+            this.AddDisconnectedHandler(options);
+            this.AddMessageReceivedHandler();
+
+            // Connect, subscribe to incoming messages and send a state message
+            await this.ConnectInternal(options);
+            await this.SubscribeInternal();
+            await this.PublishInternal(options);
+        }
+
+        /// <summary>
+        /// Stops the Sparkplug device.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        public async Task Stop()
+        {
+            await this.Client.DisconnectAsync();
+        }
+
+        /// <summary>
+        /// Loads the messages used by the the Sparkplug device.
+        /// </summary>
+        /// <param name="options">The configuration option.</param>
+        private void LoadMessages(SparkplugDeviceOptions options)
+        {
+            this.willMessage = this.MessageGenerator.CreateSparkplugMessage(
+                this.Version,
+                this.NameSpace,
+                options.GroupIdentifier,
+                SparkplugMessageType.DeviceDeath,
+                options.EdgeNodeIdentifier,
+                options.DeviceIdentifier);
+
+            this.deviceOnlineMessage = this.MessageGenerator.CreateSparkplugMessage(
+                this.Version,
+                this.NameSpace,
+                options.GroupIdentifier,
+                SparkplugMessageType.DeviceBirth,
+                options.EdgeNodeIdentifier,
+                null);
+        }
+
+        /// <summary>
+        /// Adds the disconnected handler and the reconnect functionality to the client.
+        /// </summary>
+        /// <param name="options">The configuration option.</param>
+        private void AddDisconnectedHandler(SparkplugDeviceOptions options)
+        {
+            this.Client.UseDisconnectedHandler(
+                async e =>
+                    {
+                        // Wait until the disconnect interval is reached
+                        await Task.Delay(options.ReconnectInterval);
+
+                        // Connect, subscribe to incoming messages and send a state message
+                        await this.ConnectInternal(options);
+                        await this.SubscribeInternal();
+                        await this.PublishInternal(options);
+                    });
+        }
+
+        /// <summary>
+        /// Adds the message received handler to handle incoming messages.
+        /// </summary>
+        private void AddMessageReceivedHandler()
+        {
+            this.Client.UseApplicationMessageReceivedHandler(e =>
+            {
+                // Todo: Parse client and device data here and update their states (Dictionaries here) based on the namespace
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+            });
+        }
+
+        /// <summary>
+        /// Connects the Sparkplug device to the MQTT broker.
+        /// </summary>
+        /// <param name="options">The configuration option.</param>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        private async Task ConnectInternal(SparkplugDeviceOptions options)
+        {
+            options.CancellationToken ??= CancellationToken.None;
+
+            var builder = new MqttClientOptionsBuilder()
+                .WithClientId(options.ClientId)
+                .WithCredentials(options.UserName, options.Password)
+                .WithCleanSession(false)
+                .WithProtocolVersion(MqttProtocolVersion.V311);
+
+            if (options.UseTls)
+            {
+                builder.WithTls();
+            }
+
+            if (options.WebSocketParameters is null)
+            {
+                builder.WithTcpServer(options.BrokerAddress);
+            }
+            else
+            {
+                builder.WithWebSocketServer(options.BrokerAddress, options.WebSocketParameters);
+            }
+
+            if (options.ProxyOptions != null)
+            {
+                builder.WithProxy(
+                    options.ProxyOptions.Address,
+                    options.ProxyOptions.Username,
+                    options.ProxyOptions.Password,
+                    options.ProxyOptions.Domain,
+                    options.ProxyOptions.BypassOnLocal);
+            }
+
+            if (this.willMessage != null)
+            {
+                builder.WithWillMessage(this.willMessage);
+            }
+
+            this.ClientOptions = builder.Build();
+
+            await this.Client.ConnectAsync(this.ClientOptions, options.CancellationToken.Value);
+        }
+
+        /// <summary>
+        /// Publishes data to the MQTT broker.
+        /// </summary>
+        /// <param name="options">The configuration option.</param>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        private async Task PublishInternal(SparkplugDeviceOptions options)
+        {
+            options.CancellationToken ??= CancellationToken.None;
+            await this.Client.PublishAsync(this.deviceOnlineMessage, options.CancellationToken.Value);
+        }
+
+        /// <summary>
+        /// Subscribes the client to the device subscribe topic.
+        /// </summary>
+        /// <param name="options">The configuration option.</param>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        private async Task SubscribeInternal(SparkplugDeviceOptions options)
+        {
+            var deviceCommandSubscribeTopic = this.TopicGenerator.GetDeviceCommandSubscribeTopic(this.Version, this.NameSpace, options.GroupIdentifier, options.EdgeNodeIdentifier, options.DeviceIdentifier);
+            await this.Client.SubscribeAsync(deviceCommandSubscribeTopic, MqttQualityOfServiceLevel.AtLeastOnce);
+        }
+    }
+}
