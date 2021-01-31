060f228098e47cf84bb978b44feedbb3db311470
SeppPenner <HansM2013@gmx.de>
2021-01-31 16:39:05 +0100

    Fixed naming in application header.

--- src/SparkplugNet/Application/SparkplugApplication.cs
+++ src/SparkplugNet/Application/SparkplugApplication.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SparkplugApplicationOptions.cs" company="Hämmer Electronics">
+// <copyright file="SparkplugApplication.cs" company="Hämmer Electronics">
 // The project is licensed under the MIT license.
 // </copyright>
 // <summary>
