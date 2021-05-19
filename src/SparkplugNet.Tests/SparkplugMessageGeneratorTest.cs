7bbbcd03e608e5963b999efaf9639c323e230600
SeppPenner <HansM2013@gmx.de>
2021-05-19 22:50:13 +0200

    Added test for the NCMD message.

--- src/SparkplugNet.Tests/SparkplugMessageGeneratorTest.cs
+++ src/SparkplugNet.Tests/SparkplugMessageGeneratorTest.cs
@@ -392,5 +392,53 @@ namespace SparkplugNet.Tests
             Assert.AreEqual(this.seqMetricB.LongValue, payloadVersionB.Metrics.ElementAt(1).LongValue);
             Assert.AreEqual(this.seqMetricB.Datatype, payloadVersionB.Metrics.ElementAt(1).Datatype);
         }
+
+        /// <summary>
+        /// Tests the Sparkplug message generator with a device command message with a version A namespace.
+        /// </summary>
+        [TestMethod]
+        public void TestDeviceCommandMessageNamespaceA()
+        {
+            var dateTime = DateTimeOffset.Now;
+            var message = this.messageGenerator.GetSparkPlugDeviceCommandMessage(SparkplugNamespace.VersionA, "group1", "edge1", "device1", this.metricsA, 0, 1, dateTime);
+            var payloadVersionA = PayloadHelper.Deserialize<VersionAPayload>(message.Payload);
+
+            Assert.AreEqual("spAv1.0/group1/DCMD/edge1/device1", message.Topic);
+            Assert.IsNotNull(payloadVersionA);
+            Assert.AreEqual(dateTime.ToUnixTimeMilliseconds(), payloadVersionA.Timestamp);
+            Assert.AreEqual(2, payloadVersionA.Metrics.Count);
+
+            Assert.AreEqual(this.metricsA.First().Name, payloadVersionA.Metrics.ElementAt(0).Name);
+            Assert.AreEqual(this.metricsA.First().BoolValue, payloadVersionA.Metrics.ElementAt(0).BoolValue);
+            Assert.AreEqual(this.metricsA.First().Type, payloadVersionA.Metrics.ElementAt(0).Type);
+
+            Assert.AreEqual(this.seqMetricA.Name, payloadVersionA.Metrics.ElementAt(1).Name);
+            Assert.AreEqual(this.seqMetricA.LongValue, payloadVersionA.Metrics.ElementAt(1).LongValue);
+            Assert.AreEqual(this.seqMetricA.Type, payloadVersionA.Metrics.ElementAt(1).Type);
+        }
+
+        /// <summary>
+        /// Tests the Sparkplug message generator with a device command message with a version B namespace.
+        /// </summary>
+        [TestMethod]
+        public void TestDeviceCommandMessageNamespaceB()
+        {
+            var dateTime = DateTimeOffset.Now;
+            var message = this.messageGenerator.GetSparkPlugDeviceCommandMessage(SparkplugNamespace.VersionB, "group1", "edge1", "device1", this.metricsB, 0, 1, dateTime);
+            var payloadVersionB = PayloadHelper.Deserialize<VersionBPayload>(message.Payload);
+
+            Assert.AreEqual("spBv1.0/group1/DCMD/edge1/device1", message.Topic);
+            Assert.IsNotNull(payloadVersionB);
+            Assert.AreEqual((ulong)dateTime.ToUnixTimeMilliseconds(), payloadVersionB.Timestamp);
+            Assert.AreEqual(2, payloadVersionB.Metrics.Count);
+
+            Assert.AreEqual(this.metricsB.First().Name, payloadVersionB.Metrics.ElementAt(0).Name);
+            Assert.AreEqual(this.metricsB.First().IntValue, payloadVersionB.Metrics.ElementAt(0).IntValue);
+            Assert.AreEqual(this.metricsB.First().Datatype, payloadVersionB.Metrics.ElementAt(0).Datatype);
+
+            Assert.AreEqual(this.seqMetricB.Name, payloadVersionB.Metrics.ElementAt(1).Name);
+            Assert.AreEqual(this.seqMetricB.LongValue, payloadVersionB.Metrics.ElementAt(1).LongValue);
+            Assert.AreEqual(this.seqMetricB.Datatype, payloadVersionB.Metrics.ElementAt(1).Datatype);
+        }
     }
 }
