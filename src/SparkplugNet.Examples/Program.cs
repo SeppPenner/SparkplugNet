8cf1294715d3238a495d5de09b631ae0bb91607d
SeppPenner <HansM2013@gmx.de>
2021-05-20 22:50:22 +0200

    Added example setup for version A and B application, node and method.

---
+++ src/SparkplugNet.Examples/Program.cs
@@ -0,0 +1,135 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Hämmer Electronics">
+// The project is licensed under the MIT license.
+// </copyright>
+// <summary>
+//   The main program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SparkplugNet.Examples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using SparkplugNet.Core.Application;
+    using SparkplugNet.Core.Device;
+    using SparkplugNet.Core.Node;
+
+    using VersionAPayload = VersionA.Payload;
+    using VersionBPayload = VersionB.Payload;
+
+    /// <summary>
+    /// The main program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The cancellation token source.
+        /// </summary>
+        private static readonly CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
+
+        /// <summary>
+        /// The version A metrics.
+        /// </summary>
+        private static readonly List<VersionAPayload.KuraMetric> VersionAMetrics = new List<VersionAPayload.KuraMetric>
+        {
+            new VersionAPayload.KuraMetric
+            {
+                Name = "Test", Type = VersionAPayload.KuraMetric.ValueType.Double, DoubleValue = 1.20
+            },
+            new VersionAPayload.KuraMetric
+            {
+                Name = "Test2", Type = VersionAPayload.KuraMetric.ValueType.Bool, BoolValue = true
+            }
+        };
+
+        /// <summary>
+        /// The version A metrics.
+        /// </summary>
+        private static readonly List<VersionBPayload.Metric> VersionBMetrics = new List<VersionBPayload.Metric>
+        {
+            new VersionBPayload.Metric
+            {
+                Name = "Test", Datatype = (uint)VersionBPayload.Metric.ValueOneofCase.DoubleValue, DoubleValue = 1.20
+            },
+            new VersionBPayload.Metric
+            {
+                Name = "Test2", Datatype = (uint)VersionBPayload.Metric.ValueOneofCase.BooleanValue, BooleanValue = true
+            }
+        };
+
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        public static async Task Main()
+        {
+            try
+            {
+                await RunVersionA();
+                //await RunVersionB();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Runs a version A simulation.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        private static async Task RunVersionA()
+        {
+            Console.WriteLine("Starting application...");
+            var application = new VersionA.SparkplugApplication(VersionAMetrics);
+            var applicationOptions = new SparkplugApplicationOptions("localhost", 1883, "application1", "user", "password", false, "scada1", TimeSpan.FromSeconds(30), true, null, null, CancellationTokenSource.Token);
+            await application.Start(applicationOptions);
+            Console.WriteLine("Application started...");
+
+            Console.WriteLine("Starting node...");
+            var node = new VersionA.SparkplugNode(VersionAMetrics);
+            var nodeOptions = new SparkplugNodeOptions("localhost", 1883, "node 1", "user", "password", false, "scada1", "group1", "node1", TimeSpan.FromSeconds(30), null, null, CancellationTokenSource.Token);
+            await node.Start(nodeOptions);
+            Console.WriteLine("Node started...");
+
+            Console.WriteLine("Starting device...");
+            var device = new VersionA.SparkplugDevice(VersionAMetrics);
+            var deviceOptions = new SparkplugDeviceOptions("localhost", 1883, "device 1", "user", "password", false, "scada1", "group1", "node1", "device1", TimeSpan.FromSeconds(30), null, null, CancellationTokenSource.Token);
+            await device.Start(deviceOptions);
+            Console.WriteLine("Device started...");
+        }
+
+        /// <summary>
+        /// Runs a version B simulation.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        private static async Task RunVersionB()
+        {
+            Console.WriteLine("Starting application...");
+            var application = new VersionB.SparkplugApplication(VersionBMetrics);
+            var applicationOptions = new SparkplugApplicationOptions("localhost", 1883, "application1", "user", "password", false, "scada1", TimeSpan.FromSeconds(30), true, null, null, CancellationTokenSource.Token);
+            await application.Start(applicationOptions);
+            Console.WriteLine("Application started...");
+
+            Console.WriteLine("Starting node...");
+            var node = new VersionB.SparkplugNode(VersionBMetrics);
+            var nodeOptions = new SparkplugNodeOptions("localhost", 1883, "node 1", "user", "password", false, "scada1", "group1", "node1", TimeSpan.FromSeconds(30), null, null, CancellationTokenSource.Token);
+            await node.Start(nodeOptions);
+            Console.WriteLine("Node started...");
+
+            Console.WriteLine("Starting device...");
+            var device = new VersionB.SparkplugDevice(VersionBMetrics);
+            var deviceOptions = new SparkplugDeviceOptions("localhost", 1883, "device 1", "user", "password", false, "scada1", "group1", "node1", "device1", TimeSpan.FromSeconds(30), null, null, CancellationTokenSource.Token);
+            await device.Start(deviceOptions);
+            Console.WriteLine("Device started...");
+        }
+    }
+}
